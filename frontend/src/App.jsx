f9c188c8648a1f38cd7b863de46b59e314552585
Milton <ajaxmilton@hotmail.com>
2025-11-28 13:25:33 +0200

    Add Contacts page and Add Contact page to support viewing and creating contact entries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Contacts from "./pages/Contacts";
+import AddContact from "./pages/AddContact";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/add-contact");
+  };
+
   return (
     <main className="max-w-6xl mx-auto my-5">
-      <Header />
+      <Header handleButtonClick={handleButtonClick} />
+      <Routes>
+        <Route path="/" element={<Contacts />} />
+        <Route path="/add-contact" element={<AddContact />} />
+      </Routes>
     </main>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { IoAddSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
-const Header = ({}) => {
+const Header = ({ handleButtonClick }) => {
   return (
     <header className="p-5 flex items-center justify-between bg-green-50 text-green-950">
       <h1 className="text-xl font-semibold sm:text-2xl">
@@ -8,7 +9,10 @@ const Header = ({}) => {
       </h1>
       <button
         type="button"
-        className="bg-green-950 p-1 rounded-xs hover:bg-green-600 hover:cursor-pointer"
+        className="bg-green-800 p-1 rounded-xs hover:bg-green-600 hover:cursor-pointer"
+        onClick={() => {
+          handleButtonClick();
+        }}
       >
         <IoAddSharp className="text-xl text-white " />
       </button>
